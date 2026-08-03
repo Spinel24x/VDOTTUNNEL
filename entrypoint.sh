2ef3c62e8eee63d2fa3ec4b210fea3780fcdb3b9
Spinel24x <spinelapper@gmail.com>
2026-08-03 17:00:04 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,8 @@ else
   echo "Using provided UUID: $UUID"
 fi
 
-# Replace UUID placeholder in Xray config
 sed "s/UUID_PLACEHOLDER/$UUID/" /etc/xray/config.json.template > /etc/xray/config.json
 
-# Start Python server (HTTP config page + SOCKS5 proxy)
 cd /app
 python3 main.py &
-
-# Start Xray
 /usr/local/bin/xray -config /etc/xray/config.json
