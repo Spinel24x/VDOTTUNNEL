f5a45d9979ee4fe607d227016887d0fbbc5e71ff
Spinel24x <spinelapper@gmail.com>
2026-08-03 16:48:06 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,13 @@ if [ -z "$UUID" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
   echo "Generated UUID: $UUID"
 fi
+# Save UUID for Python to read
+echo "$UUID" > /app/uuid.txt
+
+# Replace in Xray config
 sed "s/UUID_PLACEHOLDER/$UUID/" /etc/xray/config.json.template > /etc/xray/config.json
 
-# Start Python HTTP+SOCKS5 server in background
+# Start Python server (HTTP config page + SOCKS5 proxy)
 cd /app
 python3 main.py &
 
